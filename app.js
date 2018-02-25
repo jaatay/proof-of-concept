cad2502b7816589c760971111fad59682901233e
Jesse Atay <jesse.atay@gmail.com>
2018-02-25 07:23:36 -0800

    working splicer

--- app.js
+++ app.js
@@ -1,52 +1,80 @@
 'use strict'
 
-//create moment
-var testThis = moment();
+var testSection = document.getElementById('testUl');
 
-document.write(testThis + '</br>');
-document.write(testThis._d + '</br>');
+// //create moment
+// var testThis = moment();
 
-//adding to moment
-var testThisToo = moment().add(6, 'months').format();
+// document.write(testThis + '</br>');
+// document.write(testThis._d + '</br>');
 
-document.write(testThisToo + '</br>');
+// //adding to moment
+// var testThisToo = moment().add(6, 'months').format();
 
-//moment array (it turns out moments is zero-indexed, so Jan = month 0)
-var newMoment = moment([2018, 1, 5, 17]);
-var newMoment2 = moment([2018, 1, 10, 9]);
+// document.write(testThisToo + '</br>');
 
-//moment array of arrays
-var momentArray = [newMoment, newMoment2];
+// //moment array (it turns out moments is zero-indexed, so Jan = month 0)
+// var newMoment = moment([2018, 1, 5, 17]);
+// var newMoment2 = moment([2018, 1, 10, 9]);
+// var newMoment3 = moment([2019, 3, 3, 3]);
+// var newMoment4 = moment([2016, 12, 10, 22]);
+// var newMoment5 = moment([2018, 6, 4, 6]);
 
-document.write(newMoment + '</br>');
-document.write(newMoment._i[0] + '</br>');
-document.write(newMoment2.format() + '</br>');
+// //moment array of arrays
+// var momentArray = [newMoment2, newMoment, newMoment5, newMoment3, newMoment4];
 
-//moment object (still zero-indexed, Jan = month 0)
-var inThisMoment = moment({month:1, day: 5, hour: 20});
-var alsoInThisMoment = moment({month:2, day: 5, hour: 9});
-var togetherInThisMoment = [inThisMoment, alsoInThisMoment];
+// document.write(newMoment + '</br>');
+// document.write(newMoment._i[0] + '</br>');
+// document.write(newMoment2.format() + '</br>');
 
-document.write(inThisMoment.format() + '</br>');
-document.write(inThisMoment._i.day + '</br>');
+// //moment object (still zero-indexed, Jan = month 0)
+// var inThisMoment = moment({month:1, day: 5, hour: 20});
+// var alsoInThisMoment = moment({month:2, day: 5, hour: 9});
+// var togetherInThisMoment = [inThisMoment, alsoInThisMoment];
 
-//array
-var myArray = [2, 1, 4, 3, 0, 6, 7, 5];
-var sortThis = function(x, y){
-    return x - y;
+// document.write(inThisMoment.format() + '</br>');
+// document.write(inThisMoment._i.day + '</br>');
+
+// //array
+// var myArray = [2, 1, 4, 3, 0, 6, 7, 5];
+// var sortThis = function(x, y){
+//     return x - y;
+// };
+
+// document.write(myArray.sort() + '</br>');
+// myArray.push(13, 100, 0);
+
+// myArray.sort();
+// document.write(myArray + '</br>');
+
+// myArray.sort(sortThis);
+// document.write(myArray + '</br>');
+
+// myArray.push(1000, 45, 32);
+// myArray.sort(sortThis);
+// document.write(myArray + '</br>');
+
+//sort array
+var testArray = [1, 2, 3, 4, 5, 6, 7];
+
+var printThis = function(array){
+    for(var i = 0; i < array.length; i++){
+        var newLi = document.createElement('li');
+        newLi.textContent = array[i];
+        testSection.appendChild(newLi);
+        document.write('</br>');
+    }
 };
 
-document.write(myArray.sort() + '</br>');
-myArray.push(13, 100, 0);
+printThis(testArray);
 
-document.write(myArray.sort() + '</br>');
-parseInt(myArray);
+var choppedArray = testArray.splice(3);
+document.write(testArray + '</br>');
+document.write(choppedArray);
 
-myArray.sort();
-document.write(myArray + '</br>');
 
-myArray.sort(sortThis);
-document.write(myArray);
+
+
 
 
 
